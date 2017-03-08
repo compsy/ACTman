185f762f7f5e91bb5e862e838dd119d324f5d2c8
Ando Emerencia <ando.emerencia@gmail.com>
2017-03-08 13:56:21 +0100

    rubocop

--- R/actman.r
+++ R/actman.r
@@ -30,11 +30,11 @@ actman <- function(workdir) {
   ## Step 2: Loop:
   ## Loop Description: for each of the files listed in working directory, ...
   pdf("Actigraphy Data - Plot.pdf") # Initialise .pdf plotting
-  for(i in 1:length(ACTdata.files)){
+  for (i in 1:length(ACTdata.files)) {
 
     print(paste("*** Start of Dataset", i, "***"))
     print("")
-    print(paste("Dataset Name:", ACTdata.overview[i,"filename"]))
+    print(paste("Dataset Name:", ACTdata.overview[i, "filename"]))
     print("")
 
     # Read and manage data:
@@ -44,31 +44,31 @@ actman <- function(workdir) {
 
     # Reformat dates and times into required format for further processing:
     ACTdata.1.sub$Date <- gsub(pattern = "/20", replacement = "/", x = ACTdata.1.sub$Date) # Take only last two year digits
-    ACTdata.1.sub$Date <- paste(ACTdata.1.sub$Date,ACTdata.1.sub$Time) # Merge Date Time, format: "2016-01-04 20:00:00"
+    ACTdata.1.sub$Date <- paste(ACTdata.1.sub$Date, ACTdata.1.sub$Time) # Merge Date Time, format: "2016-01-04 20:00:00"
     ACTdata.1.sub$Date <- strptime(ACTdata.1.sub$Date, "%d/%m/%y %H:%M:%S") # Reformat Date
     ACTdata.1.sub$Time <- NULL # Remove empty Time variable
 
     # Check for empty first row and if so, remove it:
-    if(all(is.na(ACTdata.1.sub[1,]))){ACTdata.1.sub <- ACTdata.1.sub[-1,]} # If first row is empty, remove it.
+    if (all(is.na(ACTdata.1.sub[1, ]))) { ACTdata.1.sub <- ACTdata.1.sub[-1, ] } # If first row is empty, remove it.
 
     # Write start- and end dates and times of actigraph data to overview file:
-    ACTdata.overview[i,"start"] <- as.character(ACTdata.1.sub$Date[1]) # Write start date to overview
-    ACTdata.overview[i,"end"] <- as.character(ACTdata.1.sub$Date[nrow(ACTdata.1.sub)]) # Write end date to overview
+    ACTdata.overview[i, "start"] <- as.character(ACTdata.1.sub$Date[1]) # Write start date to overview
+    ACTdata.overview[i, "end"] <- as.character(ACTdata.1.sub$Date[nrow(ACTdata.1.sub)]) # Write end date to overview
 
     # Assign recordingtime as time difference between start and end date to overview file:
-    ACTdata.overview[i,"recordingtime"] <- round(ACTdata.1.sub$Date[1] - ACTdata.1.sub$Date[nrow(ACTdata.1.sub)], 2) # write recordingtime to overview
+    ACTdata.overview[i, "recordingtime"] <- round(ACTdata.1.sub$Date[1] - ACTdata.1.sub$Date[nrow(ACTdata.1.sub)], 2) # write recordingtime to overview
 
     # Write starting number of obs. to overview file:
-    ACTdata.overview[i,"numberofobs"] <- nrow(ACTdata.1.sub)
+    ACTdata.overview[i, "numberofobs"] <- nrow(ACTdata.1.sub)
 
     # Check for Activity in Last 5 observations:
-    ACTdata.1.sub.last <- ACTdata.1.sub$Activity[(nrow(ACTdata.1.sub)-4):nrow(ACTdata.1.sub)]
+    ACTdata.1.sub.last <- ACTdata.1.sub$Activity[(nrow(ACTdata.1.sub) - 4):nrow(ACTdata.1.sub)]
 
     # Check if Activity in Last 5 observations contain substantial activity (on average, 5 activity counts per obs.)
     ACTdata.1.sub.done <- sum(ACTdata.1.sub.last) > 5*length(ACTdata.1.sub.last)
 
     # Identify Last Whole 24 Hour component and its Position:
-    ACTdata.1.sub.lastwhole24h <- as.POSIXct(ACTdata.overview[i,"start"]) + secs14day - secsday # Last whole 24 hour component
+    ACTdata.1.sub.lastwhole24h <- as.POSIXct(ACTdata.overview[i, "start"]) + secs14day - secsday # Last whole 24 hour component
     ACTdata.1.sub.lastwhole24h.pos <- which(ACTdata.1.sub$Date == ACTdata.1.sub.lastwhole24h) # Position of Last whole 24 hour component
 
     # Summertime start and end Detection Parameters
@@ -82,7 +82,7 @@ actman <- function(workdir) {
 
     ## Summertime start Detection and 14day length correction
     print("Task 1: Summertime Start Detection and Correction")
-    if(summertime.start.indata == TRUE){
+    if (summertime.start.indata == TRUE) {
       print("Warning: Start of Summertime Detected!")
       ACTdata.1.sub.14day <- ACTdata.1.sub$Date[1] + secs14day # Start Date plus 14 days
       ACTdata.1.sub.14day <- ACTdata.1.sub.14day - secshour
@@ -90,7 +90,7 @@ actman <- function(workdir) {
       print("Action: Subtracted the one hour excess from 14day length.")
       print("Task 1 DONE: Dataset corrected for Summertime.")
       print("")
-    }else{
+    } else {
       ACTdata.1.sub.14day <- ACTdata.1.sub$Date[1] + secs14day # Start Date plus 14 days
       print("Task 1 OK: No Start of Summertime in Dataset.")
       print("")
@@ -98,14 +98,14 @@ actman <- function(workdir) {
 
     ## If Dataset is longer than Start Date plus 14 days, Remove data recorded thereafter:
     print("Task 2: Detecting if Dataset is longer than Start Date plus 14 full days")
-    if(ACTdata.1.sub[nrow(ACTdata.1.sub),"Date"] > ACTdata.1.sub.14day){
+    if (ACTdata.1.sub[nrow(ACTdata.1.sub), "Date"] > ACTdata.1.sub.14day) {
       print("Warning: Dataset is longer than Start Date plus 14 full days!")
-      ACTdata.1.sub <- ACTdata.1.sub[1:which(ACTdata.1.sub$Date == ACTdata.1.sub.14day),]
+      ACTdata.1.sub <- ACTdata.1.sub[1:which(ACTdata.1.sub$Date == ACTdata.1.sub.14day), ]
       print("Action: Observations recorded after Start Date plus 14 full days were removed.")
       print("Task 2 DONE: Dataset shortened to Start Date plus 14 days. Tasks 3, 4, and 5 also OK")
       print("")
       ACTdata.overview$lengthcheck[i] <- TRUE
-    }else{
+    } else {
       print("Task 2 OK: Dataset not longer than Start Date plus 14 days.")
       print("")
     }
@@ -113,30 +113,30 @@ actman <- function(workdir) {
 
     ## If Dataset is longer than Start Date plus 14 days minus one hour, Remove data recorded thereafter:
     print("Task 3: Detecting if Dataset is longer than Start Date plus 14 full days minus 1 hour")
-    if(ACTdata.1.sub[nrow(ACTdata.1.sub),"Date"] > (ACTdata.1.sub.14day - secshour) &
-      ((ACTdata.1.sub[nrow(ACTdata.1.sub),"Date"]-ACTdata.1.sub.14day) !=0 )){
+    if (ACTdata.1.sub[nrow(ACTdata.1.sub), "Date"] > (ACTdata.1.sub.14day - secshour) &
+      ((ACTdata.1.sub[nrow(ACTdata.1.sub), "Date"] - ACTdata.1.sub.14day) != 0)) {
       print("Warning: Data longer than Start Date plus 14 full days minus 1 hour!")
-      ACTdata.1.sub <- ACTdata.1.sub[1:which(ACTdata.1.sub$Date == (ACTdata.1.sub.14day - secshour)),]
+      ACTdata.1.sub <- ACTdata.1.sub[1:which(ACTdata.1.sub$Date == (ACTdata.1.sub.14day - secshour)), ]
       print("Action: Observations recorded after Start Date plus 14 full days minus 1 hour were removed.")
       print("Task 3 DONE: Dataset shortened to Start Date plus 14 days minus 1 hour. Tasks 4 and 5 also OK")
       print("")
       ACTdata.overview$lengthcheck[i] <- TRUE
-    }else{
+    } else {
       print("Task 3 OK: Dataset not longer than Start Date plus 14 days minus 1 hour.")
       print("")
     }
 
     ## If data longer than Start Date plus 14 days minus Two hours, Remove data recorded thereafter:
     print("Task 4: Detecting if Dataset is longer than Start Date plus 14 full days minus 2 hours")
-    if((is.na(ACTdata.overview$lengthcheck[i])) &
-       ACTdata.1.sub[nrow(ACTdata.1.sub),"Date"] > (ACTdata.1.sub.14day - (secshour*2))){
+    if ((is.na(ACTdata.overview$lengthcheck[i])) &
+       ACTdata.1.sub[nrow(ACTdata.1.sub), "Date"] > (ACTdata.1.sub.14day - (secshour*2))) {
       print("Warning: Data longer than Start Date plus 14 full days minus 2 hours!")
-      ACTdata.1.sub <- ACTdata.1.sub[1:which(ACTdata.1.sub$Date == (ACTdata.1.sub.14day - (secshour*2))),]
+      ACTdata.1.sub <- ACTdata.1.sub[1:which(ACTdata.1.sub$Date == (ACTdata.1.sub.14day - (secshour*2))), ]
       print("Action: Observations recorded after Start Date plus 14 full days were removed.")
       print("Task 4 DONE: Dataset shortened to Start Date plus 14 days minus 2 hours. Task 5 also OK")
       print("")
       ACTdata.overview$lengthcheck[i] <- TRUE
-    }else{
+    } else {
       print("Task 4 OK: Dataset not longer than Start Date plus 14 days minus 2 hours.")
       print("")
     }
@@ -144,72 +144,72 @@ actman <- function(workdir) {
     ## If data shorter than- or equal to Start Date plus 14 days minus Two hours, Skip to last Midnight:
     ## !! Must be skip to previous whole 24h !!
     print("Task 5: Detecting if Dataset is shorter than- or equal to Start Date plus 14 days minus Two hours")
-    if((is.na(ACTdata.overview$lengthcheck[i]))){
+    if ((is.na(ACTdata.overview$lengthcheck[i]))) {
       print("Warning: Data shorter than- or equal to Start Date plus 14 days minus Two hours!")
-      ACTdata.1.sub <- ACTdata.1.sub[1:ACTdata.1.sub.lastwhole24h.pos,]
+      ACTdata.1.sub <- ACTdata.1.sub[1:ACTdata.1.sub.lastwhole24h.pos, ]
       print("Action: Skipping to last whole 24h component")
       print("Task 5 DONE: Dataset Skipped to last whole 24h component")
       print("")
-    }else{
+    } else {
       print("Task 5 OK: Dataset not shorter than- or equal to Start Date plus 14 days minus Two hours.")
       print("")
     }
 
 
       # Update overview after removal
-    ACTdata.overview[i,"numberofobs2"] <- nrow(ACTdata.1.sub)
-    ACTdata.overview[i,"recordingtime2"] <- round(ACTdata.1.sub$Date[1] - ACTdata.1.sub$Date[nrow(ACTdata.1.sub)], 2)
-    ACTdata.overview[i,"end2"] <- as.character(ACTdata.1.sub$Date[nrow(ACTdata.1.sub)]) # write end date to overview
+    ACTdata.overview[i, "numberofobs2"] <- nrow(ACTdata.1.sub)
+    ACTdata.overview[i, "recordingtime2"] <- round(ACTdata.1.sub$Date[1] - ACTdata.1.sub$Date[nrow(ACTdata.1.sub)], 2)
+    ACTdata.overview[i, "end2"] <- as.character(ACTdata.1.sub$Date[nrow(ACTdata.1.sub)]) # write end date to overview
 
     ## Remove NA's
     print("Task 6: Reporting NA's")
-    ACTdata.overview[i,"missings"] <- table(is.na(ACTdata.1.sub))["TRUE"]  # write missings to overview
+    ACTdata.overview[i, "missings"] <- table(is.na(ACTdata.1.sub))["TRUE"]  # write missings to overview
     ACTdata.1.sub <- na.omit(ACTdata.1.sub)
-    print(paste("Number of NA's in this Dataset:", ACTdata.overview[i,"missings"]))
+    print(paste("Number of NA's in this Dataset:", ACTdata.overview[i, "missings"]))
     print("")
 
     ## If Activity in Last 5 observations is on average zero, Skip to Last Activity:
-    ACTdata.1.sub.last5act <- ACTdata.1.sub$Activity[(nrow(ACTdata.1.sub)-4):nrow(ACTdata.1.sub)] # Last 5 activity counts in dataset
+    ACTdata.1.sub.last5act <- ACTdata.1.sub$Activity[(nrow(ACTdata.1.sub) - 4):nrow(ACTdata.1.sub)] # Last 5 activity counts in dataset
     ACTdata.1.sub.last5act.active <- sum(ACTdata.1.sub.last5act) >= (5*length(ACTdata.1.sub.last5act)) # Is there on average more than 5 counts per obs?
     print("Task 7: Checking for Activity in Last 5 observations")
-    if(ACTdata.1.sub.last5act.active == FALSE){
+    if (ACTdata.1.sub.last5act.active == FALSE) {
       print("Warning: No Activity in Last 5 observations!")
       print("Last 5 Activity Counts, before Correction:")
       print(ACTdata.1.sub.last5act)
-      ACTdata.1.sub <- ACTdata.1.sub[1:max(which(ACTdata.1.sub$Activity >= (5*length(ACTdata.1.sub.last5act)))),] # Shortens data untill reached last activity
-      ACTdata.1.sub.last5act <- ACTdata.1.sub$Activity[(nrow(ACTdata.1.sub)-4):nrow(ACTdata.1.sub)] # Last 5 activity counts in dataset
+      ACTdata.1.sub <- ACTdata.1.sub[1:max(which(ACTdata.1.sub$Activity >= (5*length(ACTdata.1.sub.last5act)))), ] # Shortens data untill reached last activity
+      ACTdata.1.sub.last5act <- ACTdata.1.sub$Activity[(nrow(ACTdata.1.sub) - 4):nrow(ACTdata.1.sub)] # Last 5 activity counts in dataset
       ACTdata.overview$last5act.active[i] <- FALSE
       print("Last 5 Activity Counts, after Correction:")
       print(ACTdata.1.sub.last5act)
       print("Task 7 DONE: Dataset Skipped to last Activity.")
       print("")
-    }else{
+    } else {
       print("Task 7 OK: Dataset contained Activity in Last 5 observations.")
       print("")
     }
 
     # Update overview file after NA- and non-activity removal
-    ACTdata.overview[i,"numberofobs3"] <- nrow(ACTdata.1.sub)
-    ACTdata.overview[i,"recordingtime3"] <- round(ACTdata.1.sub$Date[1] - ACTdata.1.sub$Date[nrow(ACTdata.1.sub)], 2)
-    ACTdata.overview[i,"end3"] <- as.character(ACTdata.1.sub$Date[nrow(ACTdata.1.sub)]) # write end date to overview
+    ACTdata.overview[i, "numberofobs3"] <- nrow(ACTdata.1.sub)
+    ACTdata.overview[i, "recordingtime3"] <- round(ACTdata.1.sub$Date[1] - ACTdata.1.sub$Date[nrow(ACTdata.1.sub)], 2)
+    ACTdata.overview[i, "end3"] <- as.character(ACTdata.1.sub$Date[nrow(ACTdata.1.sub)]) # write end date to overview
 
     ## Use nparACT Package to calculate Experimental Variables
     wd <- getwd()
-    name <- paste(gsub(pattern = ".csv", replacement = "", x = ACTdata.files[i]),"- Cleaned Data")
-    newdir <- paste(wd,name, sep="/")
+    name <- paste(gsub(pattern = ".csv", replacement = "", x = ACTdata.files[i]), "- Cleaned Data")
+    newdir <- paste(wd, name, sep = "/")
     dir.create(newdir)
     setwd(newdir)
-    write.table(ACTdata.1.sub, row.names=FALSE, col.names = FALSE,
-                file = paste(gsub(pattern = ".csv", replacement = "", x = ACTdata.files[i]),"NEW!!!!.txt"))
+    write.table(ACTdata.1.sub, row.names = FALSE, col.names = FALSE,
+                file = paste(gsub(pattern = ".csv", replacement = "", x = ACTdata.files[i]), "NEW!!!!.txt"))
     r <- nparACT_base_loop(newdir, SR = 1/60, fulldays = T, plot = F)
     # Attach nparACT output to overview
-    ACTdata.overview[i,"IS"] <- r$IS
-    ACTdata.overview[i,"IV"] <- r$IV
-    ACTdata.overview[i,"RA"] <- r$RA
-    ACTdata.overview[i,"L5"] <- r$ L5
-    ACTdata.overview[i,"L5_starttime"] <- r$L5_starttime
-    ACTdata.overview[i,"M10"] <- r$M10
-    ACTdata.overview[i,"M10_starttime"] <- r$M10_starttime
+    ACTdata.overview[i, "IS"] <- r$IS
+    ACTdata.overview[i, "IV"] <- r$IV
+    ACTdata.overview[i, "RA"] <- r$RA
+    ACTdata.overview[i, "L5"] <- r$L5
+    ACTdata.overview[i, "L5_starttime"] <- r$L5_starttime
+    ACTdata.overview[i, "M10"] <- r$M10
+    ACTdata.overview[i, "M10_starttime"] <- r$M10_starttime
     setwd(workdir) # Reset working directory to main directory
 
     # Plot data
@@ -227,7 +227,7 @@ actman <- function(workdir) {
   ACTdata.overview$recordingtime <- ((ACTdata.overview$recordingtime)^2)^(1/2)
   ACTdata.overview$recordingtime2 <- ((ACTdata.overview$recordingtime2)^2)^(1/2)
   # Assign zero to missings without missings
-  ACTdata.overview[is.na(ACTdata.overview[,"missings"]),"missings"] <- 0
+  ACTdata.overview[is.na(ACTdata.overview[, "missings"]), "missings"] <- 0
   # Subset experimental variables
   ACTdata.1.sub.expvars <- ACTdata.overview[c("IS", "IV", "RA", "L5", "L5_starttime", "M10", "M10_starttime", "recordingtime2")]
   colnames(ACTdata.1.sub.expvars) <- c("IS", "IV", "RA", "L5", "L5 Start time", "M10", "M10 Start time", "No. of Days")
