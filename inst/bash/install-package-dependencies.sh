f4fb330dee12cc6c07824ac133925e6f1e804cae
Ando Emerencia <ando.emerencia@gmail.com>
2017-03-08 14:11:54 +0100

    also install dependencies

--- inst/bash/install-package-dependencies.sh
+++ inst/bash/install-package-dependencies.sh
@@ -3,5 +3,5 @@ options(repos = structure(c(CRAN = "http://cran-mirror.cs.uu.nl/")))
 packages <- c("testthat", "roxygen2", "nparACT", "gridExtra")
 new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
 if (length(new.packages))
-  install.packages(new.packages)
+  install.packages(new.packages, dependencies = TRUE)
 update.packages(lib.loc = Sys.getenv("R_LIBS_USER"), ask = FALSE)
