7e8c7e655561b792836dd72a62dd2b92cd1d2abb
Ando Emerencia <ando.emerencia@gmail.com>
2017-12-18 16:03:17 +0100

    fix code conventions check

--- scripts/actman_runner.R
+++ scripts/actman_runner.R
@@ -2,17 +2,12 @@
 rm(list = ls(pos = '.GlobalEnv', all.names = TRUE), pos = '.GlobalEnv')
 unloadNamespace('ACTman')
 library('ACTman', character.only = TRUE)
-setwd("/Users/ando/Documents/actman/mydata3")
+setwd("/Users/ando/Documents/actman")
 
 
 # Test with ActiWatch data
-#result <- ACTman(workdir = "/Users/ando/Documents/actman/mydata2",
-#                 sleepdatadir = "/Users/ando/Documents/actman/mydata3",
-#                 myACTdevice = "Actiwatch2",
-#                 daysperiod = 14,
-#                 endperiod = "2014-04-17 00:00:00",
-#                 startperiod = "2014-04-17 00:00:00")
+result <- ACTman(workdir = "/Users/ando/Documents/actman/mydataactiwatch", myACTdevice = "Actiwatch2")
 
 
 # Test with MW8 data
-result2 <- ACTman(workdir = "/Users/ando/Documents/actman/mydatamw8", myACTdevice = "MW8")
+# result2 <- ACTman(workdir = "/Users/ando/Documents/actman/mydatamw8", myACTdevice = "MW8")
