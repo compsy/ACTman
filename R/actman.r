564ab4b7314d5b3fdad755a143b94048dd1eeaee
Stefan Knapen <stefanknapen@MacBook-Pro-van-Stefan.local>
2018-02-19 18:27:58 +0100

    na.rm = T condition added

--- R/actman.r
+++ R/actman.r
@@ -245,7 +245,7 @@ ACTman <- function(workdir = "C:/Bibliotheek/Studie/PhD/Publishing/ACTman/R-part
 
     ## If Activity in Last 5 observations is on average zero, Skip to Last Activity:
     ACTdata.1.sub.last5act <- ACTdata.1.sub$Activity[(nrow(ACTdata.1.sub) - 4):nrow(ACTdata.1.sub)] # Last 5 activity counts in dataset
-    ACTdata.1.sub.last5act.active <- sum(ACTdata.1.sub.last5act) >= (5 * length(ACTdata.1.sub.last5act)) # Is there on average more than 5 counts per obs?
+    ACTdata.1.sub.last5act.active <- sum(ACTdata.1.sub.last5act, na.rm = T) >= (5 * length(ACTdata.1.sub.last5act)) # Is there on average more than 5 counts per obs?
     print("Task 7: Checking for Activity in Last 5 observations")
     if (ACTdata.1.sub.last5act.active == FALSE) {
       print("Warning: No Activity in Last 5 observations!")
