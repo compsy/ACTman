9a13eb1aa91d75062c16c0d93d1e2279296a3bd8
Ando Emerencia <ando.emerencia@gmail.com>
2018-04-25 02:09:25 +0200

    updated build help files script

--- inst/r/build_help.r
+++ inst/r/build_help.r
@@ -1,10 +1,10 @@
 rm(list = ls(pos = '.GlobalEnv', all.names = TRUE), pos = '.GlobalEnv')
 unloadNamespace('ACTman')
 library('ACTman', character.only = TRUE)
-unlink("/Users/ando/repos/ACTman/inst/help_files", recursive = TRUE, force = TRUE)
-dir.create("/Users/ando/repos/ACTman/inst/help_files", recursive = TRUE)
+unlink("/Users/ando/repos/ACTman/inst/docs", recursive = TRUE, force = TRUE)
 #devtools::install_github("hadley/pkgdown")
 currently_generating_help_files <<- TRUE
-pkgdown::build_site(pkg = '/Users/ando/repos/ACTman', path = "/Users/ando/repos/ACTman/inst/help_files")
+pkgdown::build_site(pkg = '/Users/ando/repos/ACTman')
 currently_generating_help_files <<- FALSE
+file.rename("/Users/ando/repos/ACTman/docs","/Users/ando/repos/ACTman/inst/docs")
 cat("Done.\n")
