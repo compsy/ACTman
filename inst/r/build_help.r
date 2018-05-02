2531aae55ff2641911eb71fd6c30f4a7eb93ae9a
Ando Emerencia <ando.emerencia@gmail.com>
2018-05-02 13:18:18 +0200

    fix specs

--- inst/r/build_help.r
+++ inst/r/build_help.r
@@ -6,5 +6,5 @@ unlink("/Users/ando/repos/ACTman/inst/docs", recursive = TRUE, force = TRUE)
 currently_generating_help_files <<- TRUE
 pkgdown::build_site(pkg = '/Users/ando/repos/ACTman')
 currently_generating_help_files <<- FALSE
-file.rename("/Users/ando/repos/ACTman/docs","/Users/ando/repos/ACTman/inst/docs")
+file.rename("/Users/ando/repos/ACTman/docs", "/Users/ando/repos/ACTman/inst/docs")
 cat("Done.\n")
